2c371c57a59d3ac53901edaf06d2a4d4d506072e
uncontrol1 <36669257+uncontrol1@users.noreply.github.com>
2020-04-28 15:41:45 +0700

    Add files via upload

---
+++ Assets/GoToTelegram.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToTelegram : MonoBehaviour {
+
+    // Use this for initialization
+    public void onClick()
+    {
+        Application.OpenURL("https://t.me/fittsu");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
